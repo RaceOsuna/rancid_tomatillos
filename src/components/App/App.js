78dfcbed8f7844e93e28ad29431b8e3be0fb97bf
Jade Shi <ziyushi1997@gmail.com>
2023-07-05 17:12:43 -0700

    Render MovieCard and MovieCardContainer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import Header from '../Header/Header';
+import MovieCardContainer from '../MovieCardContainer/MovieCardContainer';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [movieData, setMovieData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+    .then(response => response.json())
+    .then(data => setMovieData(data.movies))
+  }, []);
+
   return (
     <>
       <Header />
+      <MovieCardContainer movieData={movieData}/>
     </>
   );
 }
