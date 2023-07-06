10e0b065fff6b7ee4afdaa2ad9d78cf40debb834
Race Osuna <raceosuna@gmail.com>
2023-07-06 12:03:55 -0600

    Feat: css styling for single movie view

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from '../Header/Header';
 import MovieCardContainer from '../MovieCardContainer/MovieCardContainer';
+import SingleMovie from '../SingleMovie/SingleMovie';
 import { useState, useEffect } from 'react';
 
 function App() {
   const [movieData, setMovieData] = useState([]);
+  const [showMovie, setShowMovie] = useState(false);
+  const [clickedMovie, setClickedMovie] = useState([])
 
   useEffect(() => {
     fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
@@ -12,10 +15,24 @@ function App() {
     .then(data => setMovieData(data.movies))
   }, []);
 
+  function selectMovie(id) {
+    console.log('here')
+    fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+    .then(response => response.json())
+    .then(data => setClickedMovie(data.movie))
+
+
+    setShowMovie(true)
+  }
+
+  function goBack() {
+    setShowMovie(false)
+  }
+
   return (
     <>
-      <Header />
-      <MovieCardContainer movieData={movieData}/>
+      {!showMovie && <Header />}
+      {showMovie ? <SingleMovie clickedMovie={clickedMovie} goBack={goBack}/> : <MovieCardContainer movieData={movieData} selectMovie={selectMovie}/>}
     </>
   );
 }
