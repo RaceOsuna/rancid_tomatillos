12bee5932cec98e52f969816dfd8d27ab9868db5
Jade Shi <ziyushi1997@gmail.com>
2023-07-14 13:49:04 -0700

    Add search bar cypress test, failure message to be tested

---
+++ cypress/e2e/search_bar.cy.js
@@ -0,0 +1,47 @@
+describe('Search Ba r', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2//movies', {
+      status: 200,
+      fixture: 'mockMovies'
+    }).as('getMovies')
+
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2//movies/436270', {
+      status: 200,
+      fixture: 'movie'
+    }).as('getMovies')
+  })
+
+  it('user should be able to search', () => {
+    cy.contains('Clear Results')
+    cy.get('input[placeholder="Search Titles"]')
+    .type('black', {force: true})
+    cy.url().should('includes', '?search=black')
+    cy.get('.card').first().as('firstCard')
+    cy.get('.card').should('have.length', 2)
+    
+    cy.get('@firstCard').contains('h4', 'Black Adam')
+    cy.get('@firstCard').find('img').should('be.visible')
+    cy.get('@firstCard').contains('p', '4 ⭐️')
+
+    cy.get('.card').last().as('lastCard')
+    cy.get('@lastCard').contains('h4', 'Black Panther: Wakanda Forever')
+    cy.get('@lastCard').find('img').should('be.visible')
+    cy.get('@lastCard').contains('p', '4 ⭐️')
+
+    cy.get('@firstCard').click({multiple: true})
+    cy.url().should('includes', '/436270')
+  })
+
+  it('user should see a failure message, if the keyword does not match any movie title', () => {
+    cy.get('input[placeholder="Search Titles"]')
+    // to be implemented
+  })
+
+  it('user should be able to click the clear result button', () => {
+    cy.contains('Clear Results').click()
+    cy.url().should('eq', 'http://localhost:3000/')
+  })
+
+})
